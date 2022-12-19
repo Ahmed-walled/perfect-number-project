4fc9263cf527511e114c66878e7889fe7b2e3979
Ahmed-walled <120816217+Ahmed-walled@users.noreply.github.com>
2022-12-19 19:50:57 +0200

    Add files via upload

---
+++ perfect-number-project-new/perfect-number-project-new/Program.cs
@@ -0,0 +1,18 @@
+﻿int num, i, n1, n2;
+Console.WriteLine("enter the first number");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter the last number");
+n2 = int.Parse(Console.ReadLine());
+Console.Write("The perfect numbers between {0} and {1} are : \n", n1, n2);
+
+for ( num = n1; num <= n2; num++)
+{
+    decimal sum = 0;
+    for ( i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+            sum = sum + i;
+    }
+    if (sum == num)
+        Console.WriteLine("\t" + num);
+}
